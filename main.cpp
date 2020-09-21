7fc5ce03c96a9f966522c0aaec975eae0c2744b0
Joseph Toney <nota@joetoney.com>
2020-09-21 11:12:41 -0500

    Inital commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+template <typename T>
+struct Node {
+    T data;
+    std::unique_ptr<Node> next;
+    Node(T data) : data(data), next(nullptr){};
+    ~Node(){
+        // output on delete
+        std::cout << "Destroy node with data: " << data << std::endl; 
+    }
+};
+
+int main () {
+    auto n0{std::make_unique<Node<int>>(0)};
+    auto n1{std::make_unique<Node<int>>(1)};
+    n1->next = std::move(n0);
+}
