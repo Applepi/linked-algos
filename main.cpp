64459e458bc5b81756d82cc51f97ab520cf81f9b
Joseph Toney <nota@joetoney.com>
2020-09-21 11:36:34 -0500

    Broken code needs to fix template issue

--- main.cpp
+++ main.cpp
@@ -7,14 +7,40 @@ struct Node {
     T data;
     std::unique_ptr<Node> next = std::make_unique<Node>;
     Node(T data) : data(data), next(nullptr){};
+
     ~Node(){
         // output on delete
         std::cout << "Destroy node with data: " << data << std::endl; 
     }
 };
 
+template <typename T>
+struct List {
+    List() : head{nullptr} {};
+    void push(T data){
+        auto temp{std::make_unique< Node<T> >(data)};
+        if(head){
+            temp->next -> std::move(head);
+            head = std::move(temp);
+        } else {
+            head == std::move(temp);
+        }
+    }
+    void pop(){
+
+    }
+    ~List(){
+        // output on delete
+        std::cout << "Destroyed list" << std::endl; 
+    }
+private:
+    std::unique_ptr< Node<T> > head = std::make_unique< Node<T> >;
+};
+
 int main () {
-    auto n0{std::make_unique<Node<int>>(0)};
-    auto n1{std::make_unique<Node<int>>(1)};
-    n1->next = std::move(n0);
+    List<int> list;
+    list.push(0);
+    list.push(1);
+    list.push(2);
+    list.push(3);
 }
